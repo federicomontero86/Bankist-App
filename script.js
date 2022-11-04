2215d854bba126dd787a2c737e01b5de7c7bd2ba
federicomontero86 <104212036+federicomontero86@users.noreply.github.com>
2022-11-04 12:05:23 -0300

    Displaying the Summary
    - Incomes
    - Outcomes
    - Interest
    Using Method Chaining

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
     </div>`;
     // Adding HTML to the DOM element(containerMovements)
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -91,6 +91,31 @@ const calcDisplayBalance = function (movements) {
   labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
+
+// Displaying the Summary (IN, OUT, INTEREST)
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 // Creating the username
 const createUsernames = function (accs) {
@@ -107,7 +132,7 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 
-console.log(accounts);
+// console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -367,7 +392,7 @@ console.log(depositFor);
 
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
-*/
+
 
 // LEC: The reduce Method
 
@@ -393,3 +418,64 @@ const max = movements.reduce(
   movements[0]
 );
 console.log(max);
+
+
+Coding Challenge #2
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert 
+dog ages to human ages and calculate the average age of the dogs in their study.
+Your tasks:
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's 
+ages ('ages'), and does the following things in order:
+1. Calculate the dog age in human years using the following formula: if the dog is 
+<= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, 
+humanAge = 16 + dogAge * 4
+2. Exclude all dogs that are less than 18 human years old (which is the same as 
+keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know 
+from other challenges how we calculate averages �)
+4. Run the function for both test datasets
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK �
+
+
+const calcAverageHumanAge = function (ages) {
+  // 1
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  console.log(humanAges);
+  // 2
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(adults);
+  // 3
+  // const avg = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  // console.log(avg);
+  const avg = adults.reduce((acc, age, i, arr) => acc + age / arr.length, 0); // Different solution
+  console.log(avg);
+};
+// 4
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+*/
+
+// LEC: The Magic of Chaining Methods
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+
+// FINDING THE BUG WITH METHOD CHAINING
+// const totalDepositsUSD = movements
+//   .filter(mov => mov < 0) // Error
+//   .map((mov, i, arr) => {
+//     console.log(arr);
+//     return mov * eurToUsd;
+//   }) // With the arr parameter we can debug the previous error (accessing to the array post filter method in this case)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(totalDepositsUSD);
